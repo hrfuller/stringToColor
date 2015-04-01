880bc82953c0551edb450350b7a59b81b883a349
Tobias Schneider <schneider@synyx.de>
2015-04-01 14:41:57 +0200

    add new dist with more es6 support

--- dist/string-to-color.umd.js
+++ dist/string-to-color.umd.js
@@ -36,9 +36,31 @@
      */
     function hash(string) {
         var h = 0;
-        for (var i = 0; i < string.length; i++) {
-            h = string.charCodeAt(i) + ((h << 5) - h);
+        var _iteratorNormalCompletion = true;
+        var _didIteratorError = false;
+        var _iteratorError = undefined;
+
+        try {
+            for (var _iterator = string[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
+                var char = _step.value;
+
+                h = char.codePointAt(0) + ((h << 5) - h);
+            }
+        } catch (err) {
+            _didIteratorError = true;
+            _iteratorError = err;
+        } finally {
+            try {
+                if (!_iteratorNormalCompletion && _iterator["return"]) {
+                    _iterator["return"]();
+                }
+            } finally {
+                if (_didIteratorError) {
+                    throw _iteratorError;
+                }
+            }
         }
+
         return h;
     }
 
